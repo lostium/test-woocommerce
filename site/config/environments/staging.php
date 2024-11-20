28994213a515787e628a726624fc7eda6b710f51
Antonio García <antonio@lostium.com>
2024-11-20 07:20:17 +0100

    Adds debugging and performance configurations for staging
    Defines several constants for debugging and performance optimization in the staging environment, including query logging, debug display toggles, and fatal error handling. This enhances error tracking and improves development experience during staging.

--- site/config/environments/staging.php
+++ site/config/environments/staging.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Configuration overrides for WP_ENV === 'staging'
  */
@@ -13,5 +14,14 @@ use Roots\WPConfig\Config;
  * Example: `Config::define('WP_DEBUG', true);`
  * Example: `Config::define('DISALLOW_FILE_MODS', false);`
  */
+
+use function Env\env;
+
+Config::define('SAVEQUERIES', true);
+Config::define('WP_DEBUG', true);
+Config::define('WP_DEBUG_DISPLAY', false);
+Config::define('WP_DEBUG_LOG', env('WP_DEBUG_LOG') ?? true);
+Config::define('WP_DISABLE_FATAL_ERROR_HANDLER', true);
+Config::define('SCRIPT_DEBUG', false);
 
 Config::define('DISALLOW_INDEXING', true);
